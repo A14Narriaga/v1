dc491c48f9414ac3487f4f5f62e58606473134bf
Alan Arriaga <A14N.arriaga@gmail.com>
2021-05-10 13:24:56 -0500

    Contact modal implement

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,17 +19,17 @@ const Modal = ({ showModal, handleShowModal, language, cardSelected }:
         case 1: container = <h1>1</h1>; break;
         case 2: container = <h1>2</h1>; break;
         case 3: container = <h1>3</h1>; break;
-        case 4: container = 
-        <>
-            <CardProject 
-                title = {language === "spanish" ? Data.spa.projects[1].title : Data.eng.projects[1].title}
-                link = {Data.spa.projects[1].link}
-                git={Data.spa.projects[1].git}
-                img={Data.spa.projects[1].img}
-                desc={language === "spanish" ? Data.spa.projects[1].desc : Data.eng.projects[1].desc}
-                tools={Data.spa.projects[1].tools}
-            />
-            {/* <CardProject 
+        case 4: container =
+            <>
+                <CardProject
+                    title={language === "spanish" ? Data.spa.projects[1].title : Data.eng.projects[1].title}
+                    link={Data.spa.projects[1].link}
+                    git={Data.spa.projects[1].git}
+                    img={Data.spa.projects[1].img}
+                    desc={language === "spanish" ? Data.spa.projects[1].desc : Data.eng.projects[1].desc}
+                    tools={Data.spa.projects[1].tools}
+                />
+                {/* <CardProject 
                 title = {language === "spanish" ? Data.spa.projects[2].title : Data.eng.projects[2].title}
                 link = {Data.spa.projects[2].link}
                 git={Data.spa.projects[2].git}
@@ -37,26 +37,71 @@ const Modal = ({ showModal, handleShowModal, language, cardSelected }:
                 desc={language === "spanish" ? Data.spa.projects[2].desc : Data.eng.projects[2].desc}
                 tools={Data.spa.projects[2].tools}
             /> */}
-            <CardProject 
-                title = {language === "spanish" ? Data.spa.projects[3].title : Data.eng.projects[3].title}
-                link = {Data.spa.projects[3].link}
-                git={Data.spa.projects[3].git}
-                img={Data.spa.projects[3].img}
-                desc={language === "spanish" ? Data.spa.projects[3].desc : Data.eng.projects[3].desc}
-                tools={Data.spa.projects[3].tools}
-            />
-            <CardProject 
-                title = {language === "spanish" ? Data.spa.projects[4].title : Data.eng.projects[4].title}
-                link = {Data.spa.projects[4].link}
-                git={Data.spa.projects[4].git}
-                img={Data.spa.projects[4].img}
-                desc={language === "spanish" ? Data.spa.projects[4].desc : Data.eng.projects[4].desc}
-                tools={Data.spa.projects[4].tools}
-            />
-        </>
-        break;
+                <CardProject
+                    title={language === "spanish" ? Data.spa.projects[3].title : Data.eng.projects[3].title}
+                    link={Data.spa.projects[3].link}
+                    git={Data.spa.projects[3].git}
+                    img={Data.spa.projects[3].img}
+                    desc={language === "spanish" ? Data.spa.projects[3].desc : Data.eng.projects[3].desc}
+                    tools={Data.spa.projects[3].tools}
+                />
+                <CardProject
+                    title={language === "spanish" ? Data.spa.projects[4].title : Data.eng.projects[4].title}
+                    link={Data.spa.projects[4].link}
+                    git={Data.spa.projects[4].git}
+                    img={Data.spa.projects[4].img}
+                    desc={language === "spanish" ? Data.spa.projects[4].desc : Data.eng.projects[4].desc}
+                    tools={Data.spa.projects[4].tools}
+                />
+            </>
+            break;
         case 5: container = <h1>5</h1>; break;
-        case 6: container = <h1>6</h1>; break;
+        case 6: container =
+            <>
+                <button className="contact-btn">
+                    <i className={Data.icon.TELEGRAM}></i>
+                    <div className="contact-btnTitle">
+                        <p>Telegram</p>
+                        <p></p>
+                    </div>
+                </button>
+                <button className="contact-btn">
+                    <i className={Data.icon.WHATS}></i>
+                    <div className="contact-btnTitle">
+                        <p>WhatsApp</p>
+                        <span></span>
+                    </div>
+                </button>
+                <button className="contact-btn">
+                    <i className={Data.icon.MAIL}></i>
+                    <div className="contact-btnTitle">
+                        <p>Gmail</p>
+                        <span>A14Narriaga@gmail.com</span>
+                    </div>
+                </button>
+                <button className="contact-btn">
+                    <i className={Data.icon.TW}></i>
+                    <div className="contact-btnTitle">
+                        <p>Twitter</p>
+                        <span>@A14Narriaga</span>
+                    </div>
+                </button>
+                <button className="contact-btn">
+                    <i className={Data.icon.GITHUB}></i>
+                    <div className="contact-btnTitle">
+                        <p>GitHub</p>
+                        <span>A14Narriaga</span>
+                    </div>
+                </button>
+                <button className="contact-btn">
+                    <i className={Data.icon.IN}></i>
+                    <div className="contact-btnTitle">
+                        <p>LinkedIn</p>
+                        <span></span>
+                    </div>
+                </button>
+            </>
+            break;
     }
 
     return (
