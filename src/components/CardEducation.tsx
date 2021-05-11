0afa5251ce83d06d1f3a40a7655eb317c3a61cfd
Alan Arriaga <A14N.arriaga@gmail.com>
2021-05-11 12:54:46 -0500

    Education modal update

--- src/components/CardEducation.tsx
+++ src/components/CardEducation.tsx
@@ -1,10 +1,14 @@
-import { CSSProperties } from "react";
 
 const CardEducation = ({school, content, period, img}: 
-    {school: string, content: string, period: string, img:string}) => {
+    {school: string, content: string, period: string, img: string}) => {
+
+    const style = {
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)), url(./img/${img})`,
+    }
 
     return (
-        <section className="card-edu" style={{"--myImg": `url(./img/${img})`} as CSSProperties} >
+        <section 
+            className="card-edu" style={style} >
             <div className="edu-content">
                 <p className="edu-school">{school}</p>
                 <p className="edu-content">{content}</p>
